e7e0f7eccce4a6a9d9113d9a037a94548d7eb32e
Akshat Singhal <aksinghal@cs.stonybrook.edu>
2017-09-05 00:36:39 -0400

    new changes

--- crt/crt1.c
+++ crt/crt1.c
@@ -1,35 +1,25 @@
-//#include <stdlib.h>
 #include <sys/defs.h>
 #include <libc.h>
 
 void _start(void) {
 
-       int argc = 1;
-       char** argv = NULL;
-       char** envp = NULL;
-       uint64_t* add = NULL; 
-
+	int argc;
+        char** argv = NULL;
+        char** envp = NULL;
+        uint64_t* add = NULL; 
+	
 	__asm__(
 		"movq %%rsp, %0 \n\t"
 		:"=r"(add)
 		:
-		: 
+		:"cc", "memory"
 	);
 
-        argc = *(add + 9);
-	argv = (char **)(add + 6 + sizeof(argc));
-        envp = (char **)(argv + argc + 1);  
-	while(argc >= 0) {
-		char ch[] = {(char)(argc%10 + '0')};
-        	write(1, ch, 1);
-		argc = argc/10;
-		if(argc == 0)
-			break;
-	}
-        puts(argv[1]);
+        argc = *(add + 1);
+	argv = (char**)(add + 2);
+        envp = (char**)(argv + argc + 1);
 
-        //write(1,argv[1],1000);
-        puts("\n");
         int a = main(argc, argv, envp);
+
 	exit(a);
 }
