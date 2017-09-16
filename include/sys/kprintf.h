0d6947ad7bec1e0938afd9f864e3575b432040d9
ashuyadav93 <ashish.yadav.1@stonybrook.edu>
2017-09-16 01:05:46 -0400

    kprintf header file changes

--- include/sys/kprintf.h
+++ include/sys/kprintf.h
@@ -1,6 +1,9 @@
 #ifndef __KPRINTF_H
 #define __KPRINTF_H
 
+#include <stdarg.h>
+#include <sys/defs.h>
 void kprintf(const char *fmt, ...);
-
+void parsefmt(char *kstring, const char *fmt, va_list val);
+void convert(uint64_t a, char* ch, int base);
 #endif
